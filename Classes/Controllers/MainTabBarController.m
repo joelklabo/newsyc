28fed30d70eb386510940461b66d4ff8b7105f5b
Joel Klabo <jklabo@yammer-inc.com>
2014-12-04 15:54:12 -0800

    getting rid of non-article features

--- Classes/Controllers/MainTabBarController.m
+++ Classes/Controllers/MainTabBarController.m
@@ -14,17 +14,9 @@
 
 #import "MainTabBarController.h"
 #import "SubmissionListController.h"
-#import "SessionProfileController.h"
 #import "MoreController.h"
 #import "NavigationController.h"
-#import "ComposeController.h"
-#import "SubmissionTextComposeController.h"
-#import "SubmissionURLComposeController.h"
-#import "HackerNewsLoginController.h"
-#import "LoginController.h"
-#import "SearchController.h"
 
-#import "HNTimeline.h"
 
 @implementation MainTabBarController
 
@@ -63,21 +55,6 @@
         [latest setTitle:@"New Submissions"];
         [latest setTabBarItem:[self _tabBarItemWithTitle:@"New" imageName:@"new"]];
 
-#ifdef ENABLE_TIMELINE
-        HNEntryList *newList = [HNTimeline timelineForSession:session];
-        latest = [[[CommentListController alloc] initWithSource:newList] autorelease];
-        [latest setTitle:@"Timeline"];
-        [latest setTabBarItem:[self _tabBarItemWithTitle:[latest title] imageName:@"new"]];
-#endif
-
-        profile = [[[SessionProfileController alloc] initWithSource:[session user]] autorelease];
-        [profile setTitle:@"Profile"];
-        [profile setTabBarItem:[self _tabBarItemWithTitle:@"Profile" imageName:@"person"]];
-
-        search = [[[SearchController alloc] initWithSession:session] autorelease];
-        [search setTitle:@"Search"];
-        [search setTabBarItem:[[[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemSearch tag:0] autorelease]];
-
         more = [[[MoreController alloc] initWithSession:session] autorelease];
         [more setTitle:@"More"];
         [more setTabBarItem:[[[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemMore tag:0] autorelease]];
@@ -101,66 +78,8 @@
     return self;
 }
 
-- (void)actionSheet:(UIActionSheet *)sheet clickedButtonAtIndex:(NSInteger)index {
-    if ([[sheet sheetContext] isEqual:@"compose"]) {
-        if (index == [sheet cancelButtonIndex]) return;
-        
-        NavigationController *navigation = [[NavigationController alloc] init];
-        ComposeController *compose = nil;
-        
-        if (index == 0) {
-            compose = [[SubmissionURLComposeController alloc] initWithSession:session];
-        } else {
-            compose = [[SubmissionTextComposeController alloc] initWithSession:session];
-        }
-        
-        [navigation setViewControllers:[NSArray arrayWithObject:compose]];
-        [self presentViewController:navigation animated:YES completion:NULL];
-
-        [navigation release];
-        [compose release];
-    }
-}
-
-- (void)requestSubmissionType {
-    UIActionSheet *sheet = [[UIActionSheet alloc] init];
-    
-    [sheet addButtonWithTitle:@"Submit URL"];
-    [sheet addButtonWithTitle:@"Submit Text"];
-    [sheet addButtonWithTitle:@"Cancel"];
-    [sheet setCancelButtonIndex:2];
-    [sheet setSheetContext:@"compose"];
-    [sheet setDelegate:self];
-    
-    [sheet showFromBarButtonItemInWindow:composeItem animated:YES];
-    [sheet release];
-}
-
 - (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController {
-    BOOL isProfile = viewController == profile || ([viewController isKindOfClass:[UINavigationController class]] && [[(UINavigationController *) viewController viewControllers] objectAtIndex:0] == profile);
-
-    if (isProfile && [session isAnonymous]) {
-        [[self navigation] requestLogin];
-
-        return NO;
-	}
-    
     return YES;
-}
-
-- (void)composePressed {
-    if (![session isAnonymous]) {
-        [self requestSubmissionType];
-    } else {
-        [[self navigation] requestLogin];
-    }
-}
-
-- (void)dealloc {
-    [composeItem release];
-    [session release];
-    
-    [super dealloc];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
